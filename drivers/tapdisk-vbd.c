7130924f9e79a8234c05b21d0911c88e40c5c000
Emmanuel Varagnat <emmanuel.varagnat@vates.tech>
2026-04-16 10:09:51 +0200

    tapdisk: remove some uneeded include in VBD
    Can't remember how I stubbled upon that.

--- drivers/tapdisk-vbd.c
+++ drivers/tapdisk-vbd.c
@@ -21,15 +21,9 @@
 
 #include <stdio.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <regex.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <libgen.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/eventfd.h>
 
